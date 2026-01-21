3daaee212da24b0d975a0dda079f25749ecbc045
Baraas07 <alsayed.albaraa.h@gmail.com>
2026-01-21 19:31:37 +0300

    Refactor server to use PORT constant

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+// Serve static files (HTML)
 app.use(express.static(__dirname));
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Hotel app running");
+// Azure يوفر PORT تلقائيًا
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
